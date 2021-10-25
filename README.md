7ac69a042ba22a382b467301d7500147586dc9b9
arkedzierski <ar.kedzierski@gmail.com>
2021-10-25 22:14:37 +0200

    Added comments and update structure

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # aws_simple_app
+
+Apps in django and flask version.
+
+App RDS use PostgreSQL DB.
+
+Apps can be used with EC2 or ECS Fargate instances.
--- flask/flask_aws_rds/app.py
+++ flask/flask_aws_rds/app.py
@@ -4,23 +4,30 @@ from flask_sqlalchemy import SQLAlchemy
 import urllib
 import datetime
 import boto3
+import os
+
+RDS_ENDPOINT = os.environ.get('RDS_ENDPOINT')
+RDS_USR = os.environ.get('RDS_USER')
+RDS_DBNAME = os.environ.get('RDS_DBNAME')
 
 app = Flask(__name__)
-RDS_ENDPOINT = 'akedzierski-db.c6nhgfuujgle.us-east-2.rds.amazonaws.com'
-RDS_USR = 'ken'
-RDS_DBNAME = 'ec2'
+
+#Boto RDS client
 client = boto3.client('rds')
 token = client.generate_db_auth_token(
     DBHostname=RDS_ENDPOINT,
     Port=5432,
     DBUsername=RDS_USR
 )
+
+# Connection string for SQLAlchemy
 passwd = urllib.parse.quote(token)
 app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{RDS_USR}:{passwd}@{RDS_ENDPOINT}/{RDS_DBNAME}?sslmode=require'
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/ec'
 # app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+#SQLAlchemy db model
 db = SQLAlchemy(app)
 
 class Rds_images(db.Model):
@@ -28,14 +35,17 @@ class Rds_images(db.Model):
     name = db.Column(db.String(200))
     pub_date = db.Column(db.Time, default=datetime.datetime.now())
 
+# Default 404 if page doesn't exists
 @app.errorhandler(404)
 def page_not_found(error):
     return "Aborted with 404", 404
 
+# If / return 404
 @app.route('/')
 def root():
     abort(404)
 
+# Show db rows
 @app.route('/db')
 def index():
     images = db.session.execute('SELECT * FROM rds_images LIMIT 10;')
@@ -46,10 +56,12 @@ def index():
         images = False
     return render_template('index.html', latest_images=images)
 
+# Prepare form to add
 @app.route("/add", methods=['GET'])
 def add():
     return render_template("images_form.html")
 
+# Add text to database
 @app.route("/add", methods=['POST'])
 def imageadd():
     pname = request.form["name"]
@@ -58,8 +70,17 @@ def imageadd():
     db.session.commit()
     return redirect(url_for('index'))
 
+# Return number of rows as json
 @app.route('/num')
 def db_entry_number():
     images_num = db.session.execute('SELECT COUNT(*) FROM rds_images;').first().count
     return jsonify({'Total': images_num})
 
+# Health check for Target Group
+@app.route('/hc')
+def healt_check():
+    return "OK", 200
+
+# Run app on all i/f and port 5000
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
